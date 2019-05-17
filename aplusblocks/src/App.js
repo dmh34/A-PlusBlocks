4e89536e23f42fdbb403aa492e7aa813561f0700
peterjast <peterjast@yahoo.com>
2019-05-16 22:29:58 -0700

    pages and styling

--- aplusblocks/src/App.js
+++ aplusblocks/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Faq from './components/Faq/Faq';
+import Menu from './components/Menu/Menu';
+import Students from './components/Students/Students';
+import Grades from './components/Grades/Grades';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Route } from 'react-router-dom';
+
+class App extends Component {
+    render() {
+        return (
+          <div className="App">
+            <Header />
+              <Route exact={true} path="/" component={Home} />
+              <Route path="/faq" component={Faq} />
+              <Route path="/menu" component={Menu} />    
+              <Route path="/Students" component={Students} /> 
+              <Route path="/Grades" component={Grades} />          
+            <Footer />
+          </div>
+        )
+    }
 }
+
 
 export default App;
