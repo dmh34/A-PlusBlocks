78f77c857e73676aa37cbfd69fabe52e3cf355da
peterjast <peterjast@yahoo.com>
2019-05-18 10:57:07 -0700

    yee

--- aplusblocks/src/App.js
+++ aplusblocks/src/App.js
@@ -7,10 +7,13 @@ import Faq from './components/Faq/Faq';
 import Menu from './components/Menu/Menu';
 import Students from './components/Students/Students';
 import Grades from './components/Grades/Grades';
+import Create from './components/Create/Create';
+
 
 import { Route } from 'react-router-dom';
 
 class App extends Component {
+
     render() {
         return (
           <div className="App">
@@ -19,7 +22,8 @@ class App extends Component {
               <Route path="/faq" component={Faq} />
               <Route path="/menu" component={Menu} />    
               <Route path="/Students" component={Students} /> 
-              <Route path="/Grades" component={Grades} />          
+              <Route path="/Grades" component={Grades} /> 
+              <Route path="/Create" component={Create} />  
             <Footer />
           </div>
         )
--- aplusblocks/src/index.js
+++ aplusblocks/src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import 'babel-polyfill';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
+
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
+
+
 ReactDOM.render(
+    <Provider store = {store}>
     <BrowserRouter>
         <App />
     </BrowserRouter>
+    </Provider>
 , document.getElementById('root'));
 registerServiceWorker();
+
+
+
+
