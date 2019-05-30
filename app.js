fe46ca6b5d8b43aeca646baa64805d02c7659593
Delbert Hall <dmhalljr87@outlook.com>
2019-05-29 17:35:07 -0700

    merge conflict fixed

--- app.js
+++ app.js
@@ -7,16 +7,36 @@ const classesRoutes = require('./api/routes/classes');
 const studentRoutes = require('./api/routes/students')
 const mongoose = require('mongoose');
 
+const jwt = require("express-jwt");
+const jwtAuthz = require("express-jwt-authz");
+const jwksRsa = require("jwks-rsa");
+
+const checktJwt = jwt({
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://aplusauth.auth0.com/.well-known/jwks.json`
+    }),
+
+    // Validate the audience and the issuer.
+    audience: 'http://localhost:3001/api/students',
+    issuer: `https://aplusauth.auth0.com/`,
+    algorithms: ['RS256']
+})
+
+const checkScopes = jwtAuthz(['read:students', 'write:students']);
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/aPlusQuiz");
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin"), "*";
-    res.header("Access-Control-Allow-Headers", 
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
