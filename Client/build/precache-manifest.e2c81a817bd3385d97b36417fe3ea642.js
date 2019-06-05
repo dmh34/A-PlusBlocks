21051d4ed845d5de440192624e9c951f6b985685
Delbert Hall <dmhalljr87@outlook.com>
2019-06-05 12:16:56 -0700

    heroku deployment

---
+++ Client/build/precache-manifest.e2c81a817bd3385d97b36417fe3ea642.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3275cc78f9363b3efbffc732a32ef93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe902a142b731a97a7de",
+    "url": "/static/css/main.fb7e2648.chunk.css"
+  },
+  {
+    "revision": "797c2d0328440c507bd4",
+    "url": "/static/js/2.6dfcd105.chunk.js"
+  },
+  {
+    "revision": "fe902a142b731a97a7de",
+    "url": "/static/js/main.19e548ac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c095c4ab18d1a3f77c363581c512315c",
+    "url": "/static/media/background.c095c4ab.jpg"
+  }
+]);
