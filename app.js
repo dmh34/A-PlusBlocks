abce77571478f58cbc419e4774fd61fa8239bd78
dmh34 <dmhalljr87@outlook.com>
2019-06-06 16:08:19 -0700

    heroku

--- app.js
+++ app.js
@@ -33,19 +33,5 @@ app.use('/classes', classesRoutes);
 app.use('/students', studentRoutes);
 
 
-app.use((req, res, next) => {
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error);
-})
-
-// app.use((error, req, res, next) => {
-//     res.status(error.status || 500);
-//     res.json({
-//         error: {
-//             message: error.message
-//         }
-//     });
-// });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -16,6 +16,22 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "Client","build", "index.html"));
 });
 
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+})
+
+// app.use((error, req, res, next) => {
+//     res.status(error.status || 500);
+//     res.json({
+//         error: {
+//             message: error.message
+//         }
+//     });
+// });
+
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
