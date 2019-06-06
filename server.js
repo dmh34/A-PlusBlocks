5c55d6dd2c5b4737305cd47ea6df363ecba21493
Delbert Hall <dmhalljr87@outlook.com>
2019-06-06 14:04:55 -0700

    heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(PORT, () => {
