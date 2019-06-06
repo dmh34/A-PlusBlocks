a35331f16fcc88fe6fcb55ca7d7c473658412625
dmh34 <dmhalljr87@outlook.com>
2019-06-06 15:21:11 -0700

    heroku

--- Client/build/service-worker.js
+++ Client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.28967da0ff1162cc0ce988c880be8015.js"
+  "/precache-manifest.1cf032c8c54004cd2644f10fd931e7fc.js"
 );
 
 self.addEventListener('message', (event) => {
