d66fa95a918332bf6d9bdfa9cea44c616ddc5a11
dmh34 <dmhalljr87@outlook.com>
2019-06-06 16:06:08 -0700

    heroku

--- app.js
+++ app.js
@@ -33,11 +33,11 @@ app.use('/classes', classesRoutes);
 app.use('/students', studentRoutes);
 
 
-// app.use((req, res, next) => {
-//     const error = new Error('Not found');
-//     error.status = 404;
-//     next(error);
-// })
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
 
 // app.use((error, req, res, next) => {
 //     res.status(error.status || 500);
