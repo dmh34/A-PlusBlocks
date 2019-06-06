caa409b9cbfde6e9a5f391d8de75243020a31cab
dmh34 <dmhalljr87@outlook.com>
2019-06-06 15:27:57 -0700

    heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = require("./app");
 //app.use(checkJwt);
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("Client/build"));
+  app.use(express.static("client/build"));
 }
 
 app.get("/*", (req, res) => {
