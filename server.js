f9a0104165d882a78579a6d9c6f6e3a0bc33664c
Delbert Hall <dmhalljr87@outlook.com>
2019-06-06 13:54:56 -0700

    heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("Client/build"));
 }
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "Client", "build", "index.html"));
 });
 
